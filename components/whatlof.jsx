ed1e3589425a10c3280f29553245b7b511c0923a
rissgroup <rissgroups@gmail.com>
2022-05-27 07:05:24 -0700

    dot change

--- components/whatlof.jsx
+++ components/whatlof.jsx
@@ -8,7 +8,7 @@ export default function whatlof() {
     <div className="flex flex-col text-center w-full mb-20">
       
       <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
-      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably havent heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
     </div>
    
   </div>
