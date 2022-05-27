881217b233f792187d34ea62da11b6fe7f9c2d5d
rissgroup <rissgroups@gmail.com>
2022-05-27 04:37:16 -0700

    first commit

---
+++ components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <>
+     <h1>This is Hero</h1> 
+    </>
+  );
+}
+
+export default Hero;
+
---
+++ pages/OfficialPartner.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function OfficialPartner() {
+  return (
+    <div>OfficialPartner</div>
+  )
+}
