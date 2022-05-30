a6c853e65fca112b70d1ca885c285a0316049ada
Mahiuldin <mahiuldin95@gmail.com>
2022-05-30 19:17:37 +0500

    add-images

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Hero = () => {
   return (
     <>
-    <section className="text-white body-font pt-24">
+    <section className="text-white body-font pt-24 " >
   <div className="container mx-auto flex px-0 py-24 md:flex-row flex-col items-center">
-    <div className="lg:flex-grow md:w-1/2 md:pt-10 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+    <div className="basis-4/4 md:basis-2/4 md:pt-10   flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 className="title-font sm:text-7xl text-3xl mb-4 font-black tracking-wider text-[#D8AAF8]">Satisfying Fans.
       </h1>
       <h1 className="title-font sm:text-7xl text-3xl mb-4 font-black tracking-wider py-8 text-white">Empowering Creators.
@@ -16,8 +16,8 @@ const Hero = () => {
         
       </div>
     </div>
-    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+    <div className="basis-4/4 md:basis-2/4">
+      <img className="object-cover object-center rounded" alt="hero" src="/assets/home/hero-img.png" />
     </div>
   </div>
 </section>
--- components/Merchstore.jsx
+++ components/Merchstore.jsx
@@ -5,7 +5,7 @@ const Merchstore = () => {
     <>
   <section className="text-white body-font pt-24">
   <div className="container mx-auto flex px-0 py-24 md:flex-row flex-col items-center">
-    <div className="lg:flex-grow md:w-1/2 md:pt-10 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+    <div className="basis-4/4 md:basis-2/4 md:pt-10  flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 className="title-font sm:text-6xl text-3xl mb-4 font-bold tracking-wider  text-white">LOF</h1>
       <h1 className="title-font sm:text-6xl text-3xl mb-4 font-bold tracking-wider text-[#D8AAF8]">Merch Store </h1>
       <h1 className="title-font sm:text-2xl text-3xl mb-4 font-medium tracking-wider text-white">Always Hot Regardless of Temprature</h1>
@@ -16,8 +16,8 @@ const Merchstore = () => {
         
       </div>
     </div>
-    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+    <div className="basis-4/4 md:basis-2/4">
+      <img className="object-cover object-center rounded" alt="hero" src="/assets/home/store-img.png" />
     </div>
   </div>
 </section>
